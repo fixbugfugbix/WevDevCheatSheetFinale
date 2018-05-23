f37005a2e6a6b5354f7e19b8f134c2ac4d37c3bd
Vo Quoc Vu <LeoVo@vus-lap.rmit.edu.vn>
2018-05-23 19:53:31 +0700

    first commit

---
+++ CompleteCheatSheet/Models/Items.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompleteCheatSheet.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+    }
+
+    public class Cart
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int ProductId { get; set; }
+    }
+}
